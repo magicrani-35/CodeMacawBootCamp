f398e5fabb85b397e009a5875be9d641dc785648
magicrani-35 <babs2818@icloud.com>
2026-02-27 10:47:40 -0500

    V2.0: Practiced my for loop in Python

---
+++ file1.py
@@ -0,0 +1,9 @@
+print("Hello from file1.py!")
+
+# Tracking changes with working with git.
+lang_scripts = ['Python', 'Java', 'JavaScript', 'C#', 'Ruby', 'PHP', 'TypeScript']
+
+def print_languages():
+    for lang in lang_scripts:
+        print(lang)
+print(print_languages())
