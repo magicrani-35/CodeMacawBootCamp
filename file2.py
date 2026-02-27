71fb2141a00b92c7de3d7dff472ec440e7794cbd
magicrani-35 <babs2818@icloud.com>
2026-02-27 11:15:34 -0500

    Add language list and print top 3 languages
    Added a list of programming languages and printed the top 3.

--- file2.py
+++ file2.py
@@ -1,1 +1,7 @@
 # This is a file and for testing!
+lang_scripts = ['Python', 'Java', 'JavaScript', 'C#', 'Ruby', 'PHP', 'TypeScript']
+
+top_3 = [lang_scripts[0], lang_scripts[1], lang_scripts[3]]
+
+for scripts in lang_scripts:
+  print(top_3)
